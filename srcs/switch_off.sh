cab0fa4b7f1c3d1f9ef288ef2816ea72b209cdd5
abesombes <abesombes@protonmail.com>
2021-05-15 16:33:07 +0200

    0.7 ready to push

--- srcs/switch_off.sh
+++ srcs/switch_off.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 cp -f /etc/nginx/sites-available/localhost/nginx_off.conf /etc/nginx/sites-available/localhost/nginx.conf
 service nginx restart
+tail -f /var/log/nginx/access.log /var/log/nginx/error.log
